953342053f3581d7f00eb3f87f24d2d4ba0a7c3b
Samuel Thorson <sthor726@gmail.com>
2025-03-09 13:10:27 -0500

    more styling

--- display.py
+++ display.py
@@ -9,7 +9,6 @@ from PIL import Image, ImageDraw, ImageFont
 from lib import LCD_2inch
 import clock
 
-# Raspberry Pi pin configuration:
 RST = 27
 DC = 25
 BL = 18
@@ -25,28 +24,31 @@ try:
     image1 = Image.new("RGB", (disp.height, disp.width), "WHITE")
     draw = ImageDraw.Draw(image1)
 
-    Font1 = ImageFont.truetype("Font/Font01.ttf", 24)  
-    Font2 = ImageFont.truetype("Font/Font02.ttf", 32)  
+    Font1 = ImageFont.truetype("Font/Font00.ttf", 18)
+    Font2 = ImageFont.truetype("Font/Font01.ttf", 22)
 
     while True:
         events = clock.getCalendarEvents(5)
-        draw.rectangle([(0, 0), (disp.height, disp.width)], fill="WHITE")  
+        draw.rectangle([(0, 0), (disp.height, disp.width)], fill="WHITE")
 
         if events:
-            y_position = 10  
-            for event in events:
+            y_position = 10
+            for i, event in enumerate(events[:2]):
                 start_time = event["start"]
                 summary = event["summary"]
 
                 draw.text((10, y_position), f"Start: {start_time}", fill="BLACK", font=Font1)
-                y_position += 35  
-                
+                y_position += 25
 
                 draw.text((10, y_position), summary, fill="BLACK", font=Font2)
-                y_position += 50  
-                
-                draw.line([(10, y_position), (disp.height-10, y_position)], fill="BLACK", width=2)
-                y_position += 10  
+                y_position += 40
+
+                if i < 1:
+                    draw.line([(10, y_position), (disp.height-10, y_position)], fill="BLACK", width=1)
+                    y_position += 10
+
+            if len(events) > 2:
+                draw.text((10, y_position), "More events available...", fill="BLACK", font=Font1)
 
             disp.ShowImage(image1, 0, 0)
         else:
