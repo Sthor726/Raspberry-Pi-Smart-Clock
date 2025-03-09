a218ce1b185bb2314240e94cf1b8a7ae63279acd
Samuel Thorson <sthor726@gmail.com>
2025-03-09 13:06:27 -0500

    styling

--- display.py
+++ display.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#import chardet
 import os
-import clock
 import sys
 import time
 import logging
 import spidev as SPI
-sys.path.append("..")
+from PIL import Image, ImageDraw, ImageFont
 from lib import LCD_2inch
-from PIL import Image,ImageDraw,ImageFont
+import clock
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -18,67 +16,46 @@ BL = 18
 bus = 0
 device = 0
 logging.basicConfig(level=logging.DEBUG)
+
 try:
-    # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
     disp = LCD_2inch.LCD_2inch()
-    # Initialize library.
     disp.Init()
-    # Clear display.
     disp.clear()
 
-    # Create blank image for drawing.
-    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+    image1 = Image.new("RGB", (disp.height, disp.width), "WHITE")
     draw = ImageDraw.Draw(image1)
-    
-    # logging.info("draw point")
 
-    # draw.rectangle((5,10,6,11), fill = "BLACK")
-    # draw.rectangle((5,25,7,27), fill = "BLACK")
-    # draw.rectangle((5,40,8,43), fill = "BLACK")
-    # draw.rectangle((5,55,9,59), fill = "BLACK")
+    Font1 = ImageFont.truetype("Font/Font01.ttf", 24)  
+    Font2 = ImageFont.truetype("Font/Font02.ttf", 32)  
 
-    # logging.info("draw line")
-    # draw.line([(20, 10),(70, 60)], fill = "RED",width = 1)
-    # draw.line([(70, 10),(20, 60)], fill = "RED",width = 1)
-    # draw.line([(170,15),(170,55)], fill = "RED",width = 1)
-    # draw.line([(150,35),(190,35)], fill = "RED",width = 1)
-
-    # logging.info("draw rectangle")
-    # draw.rectangle([(20,10),(70,60)],fill = "WHITE",outline="BLUE")
-    # draw.rectangle([(85,10),(130,60)],fill = "BLUE")
-
-    # logging.info("draw circle")
-    # draw.arc((150,15,190,55),0, 360, fill =(0,255,0))
-    # draw.ellipse((150,65,190,105), fill = (0,255,0))
-
-    # logging.info("draw text")
-    #Font1 = ImageFont.truetype("/home/sthor726/Raspberry-Pi-Smart-Clock/Font/Font00.ttf",25)
-    #Font2 = ImageFont.truetype("/home/sthor726/Raspberry-Pi-Smart-Clock/Font/Font01.ttf",35)
-    #Font3 = ImageFont.truetype("/home/sthor726/Raspberry-Pi-Smart-Clock/Font/Font02.ttf",32) # Absolute path
-    Font1 = ImageFont.truetype("Font01.ttf", 25)  # Set font size
-
-    # draw.rectangle([(0,65),(140,100)],fill = "WHITE")
-    # draw.text((5, 68), 'Hello world', fill = "BLACK",font=Font1)
-    # draw.rectangle([(0,115),(190,160)],fill = "RED")
-    # draw.text((5, 118), 'WaveShare', fill = "WHITE",font=Font2)
-    # draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
-    while(True):
+    while True:
         events = clock.getCalendarEvents(5)
+        draw.rectangle([(0, 0), (disp.height, disp.width)], fill="WHITE")  
+
         if events:
+            y_position = 10  
             for event in events:
-                logging.info(event)
-                draw.text((5, 68), event, fill = "BLACK",font=Font1)
-                disp.ShowImage(image1,0,0)
-                time.sleep(5)
-                disp.clear()
+                start_time = event["start"]
+                summary = event["summary"]
+
+                draw.text((10, y_position), f"Start: {start_time}", fill="BLACK", font=Font1)
+                y_position += 35  
+                
+
+                draw.text((10, y_position), summary, fill="BLACK", font=Font2)
+                y_position += 50  
+                
+                draw.line([(10, y_position), (disp.height-10, y_position)], fill="BLACK", width=2)
+                y_position += 10  
+
+            disp.ShowImage(image1, 0, 0)
         else:
             logging.info("No upcoming events found.")
-            draw.text((5, 68), "No upcoming events found.", fill = "BLACK",font=Font1)
-            disp.ShowImage(image1,0,0)
-        
-    
+            draw.text((10, 20), "No upcoming events found.", fill="BLACK", font=Font2)
+            disp.ShowImage(image1, 0, 0)
+
+        time.sleep(5)
+
 except IOError as e:
     logging.info(e)
 except KeyboardInterrupt:
